42eaffb29b23e5494c70b58a9a4631c11f9c47f2
srichandra <147467061+dunnasrichandra@users.noreply.github.com>
2023-10-18 10:49:49 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if (n%4==0)     printf("%d",n/4);     else     printf("%d",(n/4)+1); }

--- Cars.c
+++ Cars.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    float a,c;
-    scanf("%f",&a);
-    c=a/4;
-    int b=ceil(c);
-    printf("%d",b);
+    int n;
+    scanf("%d",&n);
+    if (n%4==0)
+    printf("%d",n/4);
+    else
+    printf("%d",(n/4)+1);
 }
